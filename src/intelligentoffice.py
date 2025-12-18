6829aea2dfb0d0240346dd8bcc68b9fc9486bfe1
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2025-12-18 14:32:55 +0100

    Green Phase US3 test_manage_light_level_higher_than_550

--- src/intelligentoffice.py
+++ src/intelligentoffice.py
@@ -71,6 +71,9 @@ class IntelligentOffice:
         if self.ambient_light_sensor.lux < 500:
             GPIO.output(self.LED_PIN, GPIO.HIGH)
             self.light_on = True
+        elif self.ambient_light_sensor.lux > 550:
+            GPIO.output(self.LED_PIN, GPIO.LOW)
+            self.light_on = False
 
 
     def monitor_air_quality(self) -> None:
